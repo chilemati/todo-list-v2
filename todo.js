3a7f42a44ffa2a20f13f54bab4d7fca018b07860
chilemati <amadichile007@gmail.com>
2024-08-08 12:31:19 +0100

    first commit

--- todo.js
+++ todo.js
@@ -1,264 +1,304 @@
-// ............ Global Variables
+function intro () {
+  //? selecting or targing elements
 
-let vacantId = [];
-let existingId = [];
+// by id
+// document.action(parameter);
+let bodyById = document.getElementById('body');
+let searchInput = document.getElementById('input')
 
-function checkDeleteId() {
-	let getTodoId = document.querySelector(".tasks #todo4");
-	return getTodoId.id;
+console.log(bodyById)
+console.log(searchInput)
+
+// by query selector
+
+let bodyq = document.querySelector('#body');
+console.log(bodyq)
+
+let h1 = document.querySelector('h1');
+console.log(h1)
+
+let h2 = document.querySelector('.tasks h2');
+console.log(h2)
+
+/* 
+target in css by
+> tag name
+> class
+> id 
+> group selector
+> descendant selector
+> attribute
+>  psuedo-clasa and element
+*/
+
+// query selector all
+let allLi = document.querySelectorAll('.tasks li');
+// console.log(allLi);
+allLi.forEach(li=> {
+    console.log(li)
+})
+
+//? dom navigation
+
+ let nav = document.querySelector('.nav');
+//  let navH1 = document.querySelector('.nav h1');
+ let navH1 =nav.firstElementChild;
+ let navButton =nav.lastElementChild;
+ let navParentFromChild = navButton.parentElement;
+ let nextEle = navH1.nextElementSibling;
+ let prevEle = navButton.previousElementSibling;
+
+ console.log(nav)
+ console.log(navH1)
+ console.log(navButton)
+ console.log(navParentFromChild)
+ console.log(nextEle)
+ console.log(prevEle)
+
+//? getting and setting text
+
+// to get
+// elementVariableName.textContent
+// console.log(bodyById.innerText)
+// console.log(bodyById.textContent)
+// console.log(bodyById.innerHTML)
+console.log(navH1.textContent)
+console.log(navH1.innerText)
+console.log(navH1.innerHTML)
+// to set
+// elementVariableName.textContent = 'new text';
+//  navH1.textContent = ' TExt';
+
+ //? setting and changing attributes
+
+ // adding attribje
+//  html attribute; attributeName='value'
+//  elementVariableName.setAttribute('attributeName','attributeValue);
+ navH1.setAttribute('title','Todo List App')
+ console.log(navH1,'**********************')
+
+ // changing attriubte
+
+ nav.setAttribute('id','Nav')
+
+// ? adding css
+
+// inline css
+// variableName.style.cssPropertyName = 'value';
+/* 
+  in css 
+   
+  background-color: "blue";
+  
+  in js 
+  backgroundColor= "blue";
+
+
+*/
+navH1.style.color = 'red'
+// nav.style.backgroundColor = 'black';
+// nav.style.border = '3px dashed purple';
+
+// internal css
+
+// nav.style.cssText = `
+//   background-color: black;
+//   border: 3px dashed purple;
+// `
+
+// external css
+// classlist or setAttribute
+
+nav.classList.add('newNav');
+// nav.setAttribute('class','nav newNav');
+
+//? crating elements
+
+let li = document.createElement('li');
+let span1 = document.createElement('span');
+let span2 = document.createElement('span');
+
+// add attributes
+ span2.setAttribute('class','delete')
+ span2.setAttribute('id','todo4')
+// add text
+span1.textContent = 'New Task';
+span2.textContent = 'Delete';
+// re arrange
+
+// append add from bottom
+li.append(span1);
+li.append(span2)
+
+// prepend adds from top
+
+console.log(li)
+
+// add to dom
+
+let taskOl = document.querySelector('.tasks ol');
+
+taskOl.prepend(li)
+
+//? replace a child
+
+// create the child tag
+let newChild = document.createElement('a');
+
+// add text 
+
+newChild.textContent = 'Typescripe Lecture';
+
+// add attribute
+newChild.setAttribute('href','#');
+
+
+// select the parent tag for which a child in it will be replaced
+
+let replaceNode = document.querySelector('#todo1').parentElement;
+console.log(replaceNode);
+
+// replace the child
+
+replaceNode.replaceChild(newChild, replaceNode.childNodes[1]);
+
+
+//? remove child
+
+// select the child or the tag
+let removeNode = document.querySelector('#todo2').parentElement;
+
+// remove it
+removeNode.remove()
+
+//? html event
+
+function handleFirstLiClick (e) {
+  console.log(e)
+  alert('You Clicked Me');
 }
 
-//  ............. end of Global Variables .....
+//? dom event
 
-// alert('connected');
+/* 
+  variabeName.addEventListener('eventType',function);
+*/
 
-// selecting by getid
-let getBody = document.getElementById("body");
-// console.log(getBody);
-// getBody.style.backgroundColor='black';
-// getBody.style.color='white';
+bodyById.addEventListener('click',(e)=> {
+  alert("Dom Event Activated!!")
+  console.log(e.target);
+})
 
-// selectiing by class
 
-let getNav = document.getElementsByClassName("nav");
 
-// selecting by tagname
 
-let getLiTags = document.getElementsByTagName("li");
-// console.log(getLiTags);
-
-// get by queryselector
-
-let getOneLi = document.querySelector("li");
-// console.log(getOneLi);
-
-// set by queryselectorall
-
-let getAllLi = document.querySelectorAll("li");
-// console.log(getAllLi);
-let i = 0;
-getAllLi.forEach((x) => {
-	i++;
-	// console.log('each li: ', x.firstElementChild.textContent);
-	if (i == 2) {
-		// x.firstElementChild.innerHTML='swimming';
-	}
-	if (i == 3) {
-		// x.firstElementChild.innerHTML='Dancing';
-	}
-});
-
-// add events
-
-let getThemeBtn = document.querySelector(".nav button");
-// console.log(getThemeBtn);
-
-getThemeBtn.addEventListener("click", (e) => {
-	// console.log('You clicked me!');
-	//     getBody.style.backgroundColor='black';
-	// getBody.style.color='white';
-	// console.log(e.target)
-	// getBody.classList.toggle('darkMode');
-});
-
-// getBody.addEventListener('click', (e)=> {
-//     console.log(e.target.className);
-// })
-
-// keyup event
-
-let lastInput = document.querySelector(".lastForm input");
-// prevent default submint
-let lastForm = document.querySelector(".lastForm");
-lastForm.addEventListener("submit", (e) => {
-	e.preventDefault();
-});
-// console.log(lastInput);
-
-lastInput.addEventListener("keyup", (e) => {
-	// e.preventDefault(e);
-	let getLastInputVal = lastInput.value;
-	console.log(getLastInputVal);
-});
-
-// change event
-
-let getSelect = document.querySelector("#country");
-getSelect.addEventListener("change", (e) => {
-	let selectedOption = getSelect.value;
-	// console.log(getSelect.value);
-	switch (selectedOption) {
-		case "nig":
-			console.log("nigerian color");
-			break;
-		case "sa":
-			console.log("south-african color");
-			break;
-		case "spa":
-			console.log("spanish color");
-			break;
-		case "nig":
-			console.log("nigerian color");
-			break;
-		case "egy":
-			console.log("egyptian color");
-			break;
-		default:
-	}
-});
-// console.log(getSelect);
-
-// task one: add a text to the header
-let d = new Date();
-let date = d.toDateString().split(" ");
-
-// console.log(date.split(" "));
-// console.log(date);
-
-let getHeader = document.querySelector(".nav h1");
-getHeader.innerHTML += `for  ${date[0]}, ${date[2]} ${date[1]} ${date[3]}`;
-// console.log(getHeader.innerHTML);
-
-// task two: toggle theme between dark and white
-
-let getThemeBtn1 = document.querySelector(".nav button");
-getThemeBtn1.addEventListener("click", (e) => {
-	getBody.classList.toggle("darkMode");
-	if (getBody.className === "darkMode") {
-		getThemeBtn1.innerHTML = "Dark Theme";
-	} else {
-		getThemeBtn1.innerHTML = "Light Theme";
-	}
-	// console.log(getBody.className);
-});
-// console.log(getThemeBtn1);
-
-// task three: activate the search tasks mode
-
-let getSearchInput = document.querySelector(".search input");
-getSearchInput.addEventListener("keyup", () => {
-	let getSearchInputValue = getSearchInput.value;
-	let lowerCaseInputs = String(getSearchInputValue).toLowerCase();
-	console.log(lowerCaseInputs);
-
-	let getAllTaskLi = document.querySelectorAll(".tasks li");
-	// console.log(getAllTaskLi);
-
-	getAllTaskLi.forEach((x) => {
-		if (
-			x.firstElementChild.textContent.toLowerCase().indexOf(lowerCaseInputs) !==
-			-1
-		) {
-			//    console.log('true');
-			//    console.log(x.firstElementChild.parentElement);
-			x.firstElementChild.parentElement.style.display = "flex";
-		} else {
-			// console.log('false');
-			// console.log(x.firstElementChild.parentElement);
-			x.firstElementChild.parentElement.style.display = "none";
-		}
-		// console.log(x.firstElementChild.textContent);
-	});
-});
-// console.log(getSearchInput);
-
-showExistingTodo();
-
-// task four: deleting task;
-
-let getTaskOl = document.querySelector(".tasks ol");
-getTaskOl.addEventListener("click", (e) => {
-	// console.log(e.target.className);
-	// console.log(e.target.parentElement);
-	if (e.target.className === "delete") {
-		localStorage.removeItem(`todo${e.target.id}`);
-		//    console.log('You have deleted a task with id of : ',e.target.id);
-		e.target.parentElement.style.display = "none";
-	}
-});
-// console.log(getTaskOl);
-
-// task five: add new tasks
-let getAddTaskForm = document.querySelector(".addTask");
-
-let getAddTasksInput = document.querySelector(".addTask input");
-getAddTaskForm.addEventListener("submit", (e) => {
-	e.preventDefault();
-	// creating new tags
-	// console.log(findTodos('milky'));
-	findTodos("todo");
-	newTasks(vacantId[0], getAddTasksInput.value);
-	localStorage.setItem(`todo${vacantId[0]}`, String(getAddTasksInput.value));
-	// console.log(addTasksToOl);
-	// reset form
-	getAddTaskForm.reset();
-	// console.log(getAddTasksInput.value);
-});
-// console.log(getAddTaskForm);
-
-function newTasks(a, b) {
-	let newLi = document.createElement("li");
-	let newSpan1 = document.createElement("span");
-	let newSpan2 = document.createElement("span");
-	// add class where needed
-	newSpan2.classList.add("delete");
-	newSpan2.setAttribute("id", `${a}`);
-	// adding a none-dynamic text
-	newSpan2.innerHTML = "Delete";
-	// rearranging
-	newLi.appendChild(newSpan1);
-	newLi.appendChild(newSpan2);
-	// console.log('newLi is: ', newLi);
-	// add inputs to newspan1
-	// newSpan1.innerHTML= getAddTasksInput.value;
-	newSpan1.innerHTML = b;
-	newSpan1.style.textTransform = "capitalize";
-	// get tasks ol and append newLi as it's child
-	let addTasksToOl = document.querySelector(".tasks ol");
-	addTasksToOl.appendChild(newLi);
 }
 
-// make added todo stay on refresh
+//? activate dark mode
 
-localStorage.setItem("tod1", "used for testging1");
-console.log(localStorage.getItem("tod1"));
-localStorage.setItem("milky8", "final testing");
+/* 
+  selete body and button tag
 
-// delete intended todo
+  add event lister to button
 
-function findTodos(a) {
-	let todo;
-	let j = 0;
-	for (i = 1; i < 100; i++) {
-		j = a + i;
-		// console.log('j key is : ', j);
-		// console.log('j value is : ', localStorage.getItem(String(j)));
-		todo = localStorage.key(i);
-		// console.log('todo: ', todo);
-		if (localStorage.getItem(String(j)) != null) {
-			// console.log('i is now : ', i);
-			// console.log(' found a key match................');
-			existingId.push(i);
-		} else {
-			vacantId.push(i);
-		}
+  on click,
+   > change body tag to black bg and text to white
+   > change button text form Dark Mode to Light Mode
+  on click age revert
+*/
 
-		if (todo == null) {
-			break;
-		}
-	}
+let body = document.querySelector('body');
 
-	return vacantId;
-}
+let toggleButton = document.querySelector('.nav button');
 
-// show available tasks
-function showExistingTodo() {
-	findTodos("todo");
-	// console.log('existing is : ', existingId);
-	for (let i = 0; i < existingId.length; i++) {
-		newTasks(
-			existingId[i],
-			String(localStorage.getItem(`todo${existingId[i]}`))
-		);
-	}
-}
+// add event lister to the button
+
+toggleButton.addEventListener('click',(e)=> {
+  body.classList.toggle('darkMode');
+  if(String(body.className).includes('darkMode')) {
+    toggleButton.textContent = 'Light Mode';
+  }else{
+    toggleButton.textContent = 'Dark Mode';
+
+  }
+  console.log(body.className)
+});
+
+
+//? deleting task
+
+/* 
+  target select the task parent tag
+  add and click event
+  if the clicked tag has a class of delete
+   > get the parent of  the delete
+   > hide it
+
+*/
+
+let tasksOl = document.querySelector('.tasks ol');
+
+// click event
+
+tasksOl.addEventListener('click',(e)=> {
+  // console.log(e.target.parentElement)
+  // console.log(e.target.className)
+  if(String(e.target.className).includes('delete')) {
+    e.target.parentElement.style.display= 'none'
+  }
+})
+
+//? add tasks
+
+/* 
+  create tast list tag with the required attributes
+  select the input tag
+  get the text
+  add text
+  add attributes
+  rearrange
+  add submit event to form
+  add to dom
+  reset form
+  
+*/
+
+
+
+let addForm = document.querySelector('.addTask');
+ // submit event to form
+
+ addForm.addEventListener('submit',(e)=> {
+  e.preventDefault()
+   
+  // create tags
+
+ let li = document.createElement('li')
+ let span1 = document.createElement('span')
+ let span2 = document.createElement('span')
+
+ // select form input
+ let addInput = addForm.firstElementChild;
+
+ // add text
+ span1.textContent = addInput.value;
+ span2.textContent = 'Delete';
+let liCount = 3;
+ // add attributes
+ span2.setAttribute('class','delete');
+ span2.setAttribute('id',`todo${liCount +=1}`);
+
+ // rearrange
+
+ li.append(span1)
+ li.append(span2)
+
+   if(String(addInput.value).length >= 2) {
+     tasksOl.prepend(li);
+     addForm.reset();
+
+   }else{
+    alert('Task not Meaningfull');
+   }
+  console.log(addInput.value)
+ })
